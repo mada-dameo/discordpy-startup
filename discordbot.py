d3c7ddc061ef466c5d9c23f12be592135efc8e10
mada-dameo <77690554+mada-dameo@users.noreply.github.com>
2021-06-16 04:07:07 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,8 +3,8 @@ from discord.ext import tasks
 #from datetime import datetime
 import os
 import traceback
-bot = commands.Bot(command_prefix='/')
-token = os.environ['DISCORD_BOT_TOKEN']
+bot = commands.Bot(command_prefix="/")
+token = os.environ["DISCORD_BOT_TOKEN"]
 #channel = discord.utils.get(guild.text_channels, name="テイワット")
 callcnt = 0
 
@@ -33,10 +33,16 @@ async def on_message(message):
 
 
 @bot.command()
+async def command(ctx):
+	if command.content == "map":
+		await message.channel.send("地図を持って来てやったぞ！\nhttps://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ja-jp#/map/2?lang=ja-jp&shown_types=3,132,133,134,135,136,137,138,157,2,154,181&center=1002.58,-589.05&zoom=-2.50")
+
+	await ctx.send("地図を持って来てやったぞ！\nhttps://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ja-jp#/map/2?lang=ja-jp&shown_types=3,132,133,134,135,136,137,138,157,2,154,181&center=1002.58,-589.05&zoom=-2.50")
+"""
+@bot.command()
 async def map(ctx):
-	await ctx.send('地図を持って来てやったぞ！\nhttps://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ja-jp#/map/2?lang=ja-jp&shown_types=3,132,133,134,135,136,137,138,157,2,154,181&center=1002.58,-589.05&zoom=-2.50')
-async def code(ctx):
-	await ctx.send('ここにはお宝が隠されてるらしいぞ、早く探そうぜ！\nhttps://genshin.mihoyo.com/m/ja/gift')
+	await ctx.send("地図を持って来てやったぞ！\nhttps://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ja-jp#/map/2?lang=ja-jp&shown_types=3,132,133,134,135,136,137,138,157,2,154,181&center=1002.58,-589.05&zoom=-2.50")
+"""
 
 
 ##########################################     コマンドエラー     ###################################################
@@ -45,7 +51,7 @@ async def code(ctx):
 @bot.event
 async def on_command_error(ctx, error):
 	orig_error = getattr(error, "original", error)
-	error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+	error_msg = "".join(traceback.TracebackException.from_exception(orig_error).format())
 	await ctx.send(error_msg)
 
 
@@ -58,32 +64,33 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-
+async def code(ctx):
+	await ctx.send("ここにはお宝が隠されてるらしいぞ、早く探そうぜ！\nhttps://genshin.mihoyo.com/m/ja/gift")
 
 @bot.command()
 async def test(ctx):
     global callcnt
     callcnt += 1
     if callcnt<=3:
-        await ctx.send('オイラだぞ！ちゃんと届いてるよな？')
+        await ctx.send("オイラだぞ！ちゃんと届いてるよな？")
         #await ctx.send(callcnt)
     elif callcnt>=4:
-        await ctx.send('おい！！オイラで遊んでるだろ！')
+        await ctx.send("おい！！オイラで遊んでるだろ！")
         callcnt = 0
         #await ctx.send(callcnt)
 
 @bot.command()
 async def phelp(ctx):
-    await ctx.send('```\n/map\n/code\n/test\n```')
+    await ctx.send("```\n/map\n/code\n/test\n```")
 
 @tasks.loop(seconds=60)
 async def loop():
     # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
+    now = datetime.now().strftime("%H:%M")
     print(now)
-    if now == '20:00':
+    if now == "20:00":
         channel = client.get_channel(CHANNEL_ID)
-        await channel.send('@everyone \n今日のWebログインは受け取ったか？\nURLを貼っておいてやるから感謝しろよな！\nhttps://webstatic-sea.mihoyo.com/ys/event/signin-sea/index.html?act_id=e202102251931481\n')  
+        await channel.send("@everyone \n今日のWebログインは受け取ったか？\nURLを貼っておいてやるから感謝しろよな！\nhttps://webstatic-sea.mihoyo.com/ys/event/signin-sea/index.html?act_id=e202102251931481\n")  
 
 """
 bot.run(token)
