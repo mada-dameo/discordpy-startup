f4634db78b25ff7414641b22c5b1a8920d3593f2
mada-dameo <77690554+mada-dameo@users.noreply.github.com>
2021-06-16 02:49:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,11 +3,9 @@ from discord.ext import tasks
 #from datetime import datetime
 import os
 import traceback
-
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-channel = discord.utils.get(guild.text_channels, name="テイワット")
-
+#channel = discord.utils.get(guild.text_channels, name="テイワット")
 callcnt = 0
 
 
@@ -15,9 +13,9 @@ callcnt = 0
 
 @bot.event
 async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+	orig_error = getattr(error, "original", error)
+	error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+	await ctx.send(error_msg)
 
 
 ##########################################     会話     ###################################################
@@ -25,26 +23,25 @@ async def on_command_error(ctx, error):
 
 @bot.event
 async def on_message(message):
-    """メッセージを処理"""
-    if message.author.bot:  # ボットのメッセージをハネる
-        return
-
-    if message.content == "えへっ。":
-        # チャンネルへメッセージを送信
-        await message.channel.send("「えへっ」ってなんだよ…！！")
+	"""メッセージを処理"""
+	if message.author.bot:  # ボットのメッセージをハネる
+		return
+	
+	if message.content == "えへっ。":
+		# チャンネルへメッセージを送信
+		await message.channel.send("「えへっ」ってなんだよ…！！")
 
 
 ##########################################     コマンド     ###################################################
 
 
 
-    if message.author.bot:  # ボットのメッセージをハネる
-        return
-
-    if message.content == "/map":
-        # チャンネルへメッセージを送信
-        await message.channel.send('地図を持って来てやったぞ！\nhttps://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ja-jp#/map/2?lang=ja-jp&shown_types=3,132,133,134,135,136,137,138,157,2,154,181&center=1002.58,-589.05&zoom=-2.50')
-
+	if message.author.bot:  # ボットのメッセージをハネる
+		return
+	
+	if message.content == "/map":
+		# チャンネルへメッセージを送信
+		await message.channel.send('地図を持って来てやったぞ！\nhttps://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ja-jp#/map/2?lang=ja-jp&shown_types=3,132,133,134,135,136,137,138,157,2,154,181&center=1002.58,-589.05&zoom=-2.50')
 """
 @bot.command()
 async def map(ctx):
