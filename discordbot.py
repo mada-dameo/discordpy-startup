eb4496ef7177808015018603a33a5b5c96ea6a04
mada-dameo <77690554+mada-dameo@users.noreply.github.com>
2021-06-15 23:22:11 +0900

    change test

--- discordbot.py
+++ discordbot.py
@@ -1,7 +1,10 @@
 from discord.ext import commands
+from discord.ext import tasks
+from datetime import datetime
+import discord
 import os
 import traceback
-
+int cnt = 0
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
@@ -12,9 +15,17 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     await ctx.send(error_msg)
 
+@bot.command()
+async def map(ctx):
+    await ctx.send('地図を持って来てやったぞ！\nhttps://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ja-jp#/map/2?lang=ja-jp&shown_types=3,132,133,134,135,136,137,138,157,2,154,181&center=1002.58,-589.05&zoom=-2.50')
 
 @bot.command()
 async def test(ctx):
-    await ctx.send('pong')
+    cnt++
+    if cnt<3:
+        await ctx.send('オイラだぞ！ちゃんと届いてるよな？')
+    else
+        cnt = 0
+        await ctx.send('……おい！オイラで遊んでるだろ！！')
 
 bot.run(token)
