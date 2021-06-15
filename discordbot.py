dde1e0f0af72871574d9824ee87912119db9bab4
mada-dameo <77690554+mada-dameo@users.noreply.github.com>
2021-06-16 03:00:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -31,15 +31,16 @@ async def on_message(message):
 
 ##########################################     コマンド     ###################################################
 
+@bot.command()
+async def map(ctx):
+	await ctx.send('地図wo持って来てやったぞ！\nhttps://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ja-jp#/map/2?lang=ja-jp&shown_types=3,132,133,134,135,136,137,138,157,2,154,181&center=1002.58,-589.05&zoom=-2.50')
+
 """	
 	if message.content == "/map":
 		# チャンネルへメッセージを送信
 		await message.channel.send('地図を持って来てやったぞ！\nhttps://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ja-jp#/map/2?lang=ja-jp&shown_types=3,132,133,134,135,136,137,138,157,2,154,181&center=1002.58,-589.05&zoom=-2.50')
-"""
-@bot.command()
-async def map(ctx):
-	await ctx.send('地図wo持って来てやったぞ！\nhttps://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ja-jp#/map/2?lang=ja-jp&shown_types=3,132,133,134,135,136,137,138,157,2,154,181&center=1002.58,-589.05&zoom=-2.50')
-"""
+
+
 @bot.command()
 async def code(ctx):
     await ctx.send('ここにはお宝が隠されてるらしいぞ、早く探そうぜ！\nhttps://genshin.mihoyo.com/m/ja/gift')
