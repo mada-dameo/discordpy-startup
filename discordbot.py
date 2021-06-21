5f565a2ddd79152740c9aba6f2ffe5ff88ca1efb
mada-dameo <77690554+mada-dameo@users.noreply.github.com>
2021-06-21 22:05:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -6,7 +6,7 @@ import os
 import traceback
 bot = commands.Bot(command_prefix="/")
 token = os.environ["DISCORD_BOT_TOKEN"]
-channel = client.get_channel(853870451535904799)
+channel = bot.get_channel(853870451535904799)
 #channel = discord.utils.get(guild.text_channels, name="テイワット")
 callcnt = 0
 
