e6784dc9b95b48c84a09e44d4d7671ae3020c73d
mada-dameo <77690554+mada-dameo@users.noreply.github.com>
2021-08-26 18:13:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,89 +1,28 @@
-
-from discord.ext import commands
-from discord.ext import tasks
+import os
 import discord
 import datetime
-import os
-import traceback
-
-bot = commands.Bot(command_prefix="/")
-token = os.environ.["DISCORD_BOT_TOKEN"]
-ch = os.environ["CHANNEL_ID"]
-#channel = discord.utils.get(guild.text_channels, name="テイワット")
-callcnt = 0
+import pytz
+from discord.ext import tasks
+from asyncio import sleep
 
 
+TOKEN = "ODUzODIxMzM4NTM1MzI5ODEy.YMa9Fg.7UCsHcdkP84CXe_HUH2MC0xkCDo"
+CHANNEL_ID = 854407350084960297
 
-##########################################     会話     ###################################################
+client = discord.Client()
 
-
-@bot.event
-async def on_message(message):
-	"""メッセージを処理"""
-	if message.author.bot:  # ボットのメッセージをハネる
-		return
-	if message.content == "えへっ。":
-		await message.channel.send("「えへっ」ってなんだよ…！！")
-
-
-
-	await bot.process_commands(message)
-
-##########################################     コマンド     ###################################################
-
-
-@bot.command()
-async def map(ctx):
-	await ctx.send("地図を持って来てやったぞ！\nhttps://webstatic-sea.mihoyo.com/app/ys-map-sea/index.html?lang=ja-jp#/map/2?lang=ja-jp&shown_types=3,132,133,134,135,136,137,138,157,2,154,181&center=1002.58,-589.05&zoom=-2.50")
-@bot.command()
-async def code(ctx):
-	await ctx.send("ここにはお宝が隠されてるらしいぞ、早く探そうぜ！\nhttps://genshin.mihoyo.com/m/ja/gift")
-@bot.command()
-async def test(ctx):
-    global callcnt
-    callcnt += 1
-    if callcnt<=3:
-        await ctx.send("オイラだぞ！ちゃんと届いてるよな？")
-        #await ctx.send(callcnt)
-    elif callcnt>=4:
-        await ctx.send("おい！！オイラで遊んでるだろ！")
-        callcnt = 0
-        #await ctx.send(callcnt)
-@bot.command()
-async def paimon(ctx):
-	await ctx.send("```\n/map\n/code\n/test\n```")
-
-@bot.command()
-async def time(ctx):
-	now = datetime.datetime.now().strftime('%H:%M')
-	await ctx.send(now)
-
-@bot.command()
-async def ch(ctx):
-	channel = bot.get_channel(ch)
-	await ctx.send(channel)
-
-
-##########################################     コマンドエラー     ###################################################
-
-
-@bot.event
-async def on_command_error(ctx, error):
-	orig_error = getattr(error, "original", error)
-	error_msg = "".join(traceback.TracebackException.from_exception(orig_error).format())
-	await ctx.send(error_msg)
-
-
-##########################################     タスク     ###################################################
-
+# 60秒に一回ループ
 @tasks.loop(seconds=60)
 async def loop():
-	now = datetime.datetime.now().strftime('%H:%M')
-	#channel = bot.get_channel["CHANNEL_ID"]
-	#channel = bot.get_channel["CHANNEL_ID"]
-	#if now == '22:41':
-	channel = bot.get_channel(int(ch))
-	await channel.send("loopが回ったぞ")
-loop.start()
+    now = datetime.datetime.now(pytz.timezone('Asia/Tokyo')).strftime('%H:%M')
+    channel = client.get_channel(CHANNEL_ID)
+    print(now)
+    print(channel)
+    await channel.send('時間だよ')  
 
-bot.run(token)
+#ループ処理実行
+loop.start()
+if TOKEN is None or CHANNEL_ID is None:
+    print('環境変数が適切に設定されていません')
+else:
+    client.run(TOKEN)
